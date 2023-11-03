a9e3c9f59650a48bb38e8619b16f9bafd52a5fff
Chhokmah00 <5m4wu6@gmail.com>
2023-11-03 11:54:51 +0800

    week08

--- week08/week08-6.py
+++ week08/week08-6.py
@@ -2,11 +2,11 @@ class Solution:
     def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
 
         N=len(arr)
-        for k in range(N):
-            for i in range(1,N):
-                if arr[i]<arr[i-1]:
-                    arr[i],arr[i-1]=arr[i-1],arr[i]
-
+        #for k in range(N):
+            #for i in range(1,N):
+                #if arr[i]<arr[i-1]:
+                    #arr[i],arr[i-1]=arr[i-1],arr[i]
+        arr.sort()
         for i in range(1,N):
             if arr[i]-arr[i-1]!=arr[1]-arr[0]:
                 return False
